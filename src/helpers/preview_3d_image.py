1133a6e9ef3d924f008e5d2791f21a861516a84e
Nikolas Tomastik <nikolas.tsk@gmail.com>
2021-04-06 17:21:58 +0200

    Fixed preview_3d_image

--- src/helpers/preview_3d_image.py
+++ src/helpers/preview_3d_image.py
@@ -6,7 +6,7 @@ from IPython.display import display
 from ipywidgets import widgets
 
 
-def preview_3d_image(img, figsize=(16, 16)):
+def preview_3d_image(img, figsize=(16, 16), show_unique=False):
     if type(img) is sitk.SimpleITK.Image:
         img = sitk.GetArrayFromImage(img)
 
@@ -16,8 +16,10 @@ def preview_3d_image(img, figsize=(16, 16)):
         plt.figure(figsize=figsize)
         plt.imshow(img[slice_index])
         plt.show()
-        print(f"debug: {img.min()}, {img.max()}")
-        print(f"debug: unique {np.unique(img[slice_index])}")
+
+        if show_unique:
+            print(f"debug: {img.min()}, {img.max()}")
+            print(f"debug: unique {np.unique(img[slice_index])}")
 
     sliceSlider = widgets.IntSlider(min=0, max=max_slices - 1, step=1, value=(max_slices - 1) / 2)
     ui = widgets.VBox([widgets.HBox([sliceSlider])])
