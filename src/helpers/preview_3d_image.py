eb1697e4c75a6da23c2c6ed2f2c1b89cf75f5311
Nikolas Tomastik <nikolas.tsk@gmail.com>
2021-05-04 17:27:56 +0200

    Updated preview 3D image

--- src/helpers/preview_3d_image.py
+++ src/helpers/preview_3d_image.py
@@ -6,7 +6,7 @@ from IPython.display import display
 from ipywidgets import widgets
 
 
-def preview_3d_image(img, figsize=(16, 16), show_unique=False):
+def preview_3d_image(img, figsize=(16, 16), show_slice=None, show_unique=False, minmax=False):
     if type(img) is sitk.SimpleITK.Image:
         img = sitk.GetArrayFromImage(img)
 
@@ -14,14 +14,21 @@ def preview_3d_image(img, figsize=(16, 16), show_unique=False):
 
     def f(slice_index):
         plt.figure(figsize=figsize)
-        plt.imshow(img[slice_index])
+        if minmax:
+            plt.imshow(img[slice_index], vmin=np.min(img), vmax=np.max(img))
+        else:
+            plt.imshow(img[slice_index])
         plt.show()
 
         if show_unique:
             print(f"debug: {img.min()}, {img.max()}")
             print(f"debug: unique {np.unique(img[slice_index])}")
 
-    sliceSlider = widgets.IntSlider(min=0, max=max_slices - 1, step=1, value=(max_slices - 1) / 2)
+    if show_slice:
+        sliceSlider = widgets.IntSlider(min=0, max=max_slices - 1, step=1, value=show_slice)
+    else:
+        sliceSlider = widgets.IntSlider(min=0, max=max_slices - 1, step=1, value=(max_slices - 1) / 2)
+
     ui = widgets.VBox([widgets.HBox([sliceSlider])])
     out = widgets.interactive_output(f, {'slice_index': sliceSlider})
     # noinspection PyTypeChecker
